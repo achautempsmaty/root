d01d3d19e2aef7575148fc09af1e0b09ad05fc80
achautempsmaty <66299243+achautempsmaty@users.noreply.github.com>
2022-03-10 14:57:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const directoryPath = path.resolve(rootPath);
 const logPercent = (i, length, isConvert) => {
   let label = isConvert
     ? '\x1b[34m' + 'Conversion des fichiers' + '\x1b[0m'
-    : 'Analyse des dossiers';
+    : '\x1b[34m' + 'Dossiers analysés' + '\x1b[0m';
 
   console.log(
     label,
@@ -17,89 +17,76 @@ const logPercent = (i, length, isConvert) => {
 };
 
 let informations = {
-  errors: [],
-  numberFile: 0,
+  imagesprocessed: 0,
   temporary: 0,
+  errors: 0,
 };
 
-const filesToProcess = [];
-
-function endScript() {
-  console.log('Nombre de fichiers convertis : ' + informations.numberFile);
+function success(result) {
+  informations = {
+    ...informations,
+    imagesprocessed: informations.imagesprocessed + 1,
+  };
   console.log(
-    'Nombre de Dossier Temporary supprimés : ' + informations.temporary
+    '\x1b[32m' +
+      'Nombre de fichiers convertis' +
+      '\x1b[0m :' +
+      '\x1b[1m' +
+      informations.imagesprocessed +
+      '\x1b[0m'
   );
-  if (informations.errors.length > 0) {
-    let label = informations.errors.length === 1 ? 'erreur' : 'erreurs';
-    console.log(
-      informations.errors.length + ' ' + label + ' dans le traitement'
-    );
-    informations.errors.forEach((error) => console.log(error));
-  } else {
-    console.log('Aucune erreur dans le traitement des images');
-  }
+}
+function error(err) {
+  informations = {
+    ...informations,
+    errors: informations.errors + 1,
+  };
+  console.log(
+    '\x1b[31m' +
+      "Nombre d'erreur" +
+      '\x1b[0m : ' +
+      '\x1b[1m' +
+      informations.errors +
+      '\x1b[0m'
+  );
+  console.log(err);
 }
 
-const processImage = async (file) => {
-  return new Promise((resolve, reject) => {
-    sharp(file.jpg)
-      .webp({
-        quality: webpQuality,
-      })
-      .toFile(file.webp)
-      .then((result) => resolve(result))
-      .catch((err) => reject(err));
-  });
-};
-
-function getFiles(directory) {
+function checkFiles(directory) {
   const filesInDirectory = fs.readdirSync(directory);
   filesInDirectory.forEach(async (file, index) => {
     const pathname = path.join(directory, file);
     if (fs.statSync(pathname).isDirectory() && file !== 'temporary') {
       logPercent(index, filesInDirectory.length, false);
-      getFiles(pathname);
+      checkFiles(pathname);
     } else if (fs.statSync(pathname).isDirectory() && file == 'temporary') {
-      this.informations = {
-        ...this.informations,
-        temporary: this.informations.temporary + 1,
+      informations = {
+        ...informations,
+        temporary: informations.temporary + 1,
       };
-      fs.rmSync(pathname, { recursive: true, force: true });
+      fs.rmSync(pathname, {
+        recursive: true,
+        force: true,
+      });
+      console.log(
+        'Nombre de Dossier Temporary supprimés : ' + informations.temporary
+      );
     } else {
       if (path.extname(pathname).toLowerCase() == '.jpg') {
         const jpg = pathname;
         const webp = jpg.replace('jpg', 'webp').replace('JPG', 'webp');
         if (!fs.existsSync(webp)) {
-          filesToProcess.push({ jpg, webp });
+          sharp(jpg)
+            .webp({
+              quality: webpQuality,
+            })
+            .toFile(webp)
+            .then((result) => success(result))
+            .catch((err) => error(err));
         }
       }
     }
   });
 }
 
-getFiles(directoryPath, false);
-
-(async () => {
-  let count = 0;
-  const promises = filesToProcess.map((file, index) =>
-    processImage(file)
-      .then(() => {
-        informations = {
-          ...informations,
-          numberFile: informations.numberFile + 1,
-        };
-      })
-      .catch((err) => {
-        informations = {
-          ...informations,
-          errors: [...informations.errors, err],
-        };
-      })
-      .then(() => {
-        logPercent(count, filesToProcess.length, true);
-        count++;
-      })
-  );
-  await Promise.all(promises);
-  endScript();
-})();
+checkFiles(directoryPath, false);
